804ba645e177c73631533176eac89d0fae71a2ce
Elias Ribeiro <eliasedu13.rs@gmail.com>
2024-06-02 17:31:59 -0300

    novo projeto

---
+++ smart_tv/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Ligada: " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume da TV: " + smartTv.volume);
+    }
+}
+
+//INSIRA AS AÇÔES A SEREM REALIZADAS COM
+//BASE NOS MÉTODOS DA SMART TV.
