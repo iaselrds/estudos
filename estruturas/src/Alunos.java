1cf0acd10d0692766f03a37767fa37bb811f89eb
Elias Ribeiro <eliasedu13.rs@gmail.com>
2024-06-11 13:50:58 -0300

    DesafioControleFluxo

---
+++ estruturas/src/Alunos.java
@@ -0,0 +1,9 @@
+public class Alunos {
+    public static void main(String[] args) throws Exception {
+        String alunos [] = {"Elias", "Erick", "Aline", "Lucas", "Eva"};
+
+        for(String aluno : alunos){
+            System.out.println("O aluno atual é "+ aluno);
+        }
+    }
+}
