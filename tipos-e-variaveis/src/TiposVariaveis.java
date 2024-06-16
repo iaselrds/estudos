cf85922e1d26a9976fcae340a9f92afda9b405dc
Elias Ribeiro <89391030+iaselrds@users.noreply.github.com>
2024-06-15 21:49:04 -0300

    Delete tipos-e-variaveis directory

--- tipos-e-variaveis/src/TiposVariaveis.java
+++
@@ -1,7 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        final double VALOR_DE_PI = 3.14;
-
-        System.out.println(VALOR_DE_PI);
-    }
-}
