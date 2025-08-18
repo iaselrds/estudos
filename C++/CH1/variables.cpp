4acae1c58c5c8a630b106c64892be7dabbff3e05
Elias <elias.ribeiro.silva@itec.ufpa.br>
2025-08-18 14:42:07 -0300

    Nothing worth noting

--- C++/CH1/variables.cpp
+++ C++/CH1/variables.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 
 int main(){
-    int width;
-    width = 5;
-
-    std::cout << width;
-
-    width = 7;
-
-    std::cout << width;
+    [[maybe_unused]] int width = 0;
 
     return 0;
 }
