4b5b589c032c2c96bf09f0f830ce7f160e561806
Elias Ribeiro <eliasedu13.rs@gmail.com>
2025-11-20 12:10:34 -0300

    Car rental system developed with no interfaces in Java

---
+++ java/Curso_Java_Parte2/src/model/services/RentalService.java
@@ -0,0 +1,59 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService tax) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = tax;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public BrazilTaxService getTax() {
+		return taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes/60;
+		double basicPayment;
+		double tax;
+		
+		if (hours <= 12) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		} else {
+			basicPayment = pricePerDay * Math.ceil(hours/24);
+		}
+		
+		tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+}
