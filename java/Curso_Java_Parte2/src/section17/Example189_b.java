c02ad75217e4ddeffec3fa26f338f42b3bf5265b
Elias Ribeiro <eliasedu13.rs@gmail.com>
2026-04-06 02:25:12 -0300

    Exercise about wildcards concepts (covariance and contravariance).

--- java/Curso_Java_Parte2/src/section17/Example189_b.java
+++ java/Curso_Java_Parte2/src/section17/Example189_b.java
@@ -1,4 +1,35 @@
 package section17;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Example189_b {
+
+    public static void main(String[] args) {
+
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
+
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+
+    }
+
+    public static void copy(List<? extends Number> origem, List<? super Number> destino) {
+        for (Number number : origem){
+            destino.add(number);
+        }
+    }
+
+    public static void printList(List<?> list) {
+        for (Object obj : list){
+            System.out.print(obj + " ");
+        }
+
+        System.out.println();
+    }
 }
